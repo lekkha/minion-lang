4966ae6d3985d33b650f62c67f890d95eb9e569a
Lekha <lekhasoni001@gmail.com>
2023-12-04 14:17:17 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate"); 
-//select the input box
 var txtInput = document.querySelector("#txt-input")
-//check if it has selected the textbox via outputing it in the console
-console.log(txtInput)
-//reading the input value 
+var outputdiv = document.querySelector("#output")
 
 function clickHandler(){
-    console.log("clicked!");
-    console.log("input" , txtInput.value);
+    outputdiv.innerText = "banana language translation " + txtInput.value 
+    
 }
 btnTranslate.addEventListener("click", clickHandler)
     
