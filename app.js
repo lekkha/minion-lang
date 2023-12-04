9e6ccb61ac17fcbbdfa763f1eca6cd418c8278cf
Lekha <lekhasoni001@gmail.com>
2023-12-04 12:29:40 +0530

    input

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var btnTranslate = document.querySelector("#btn-translate"); 
+//select the input box
+var txtInput = document.querySelector("#txt-input")
+//check if it has selected the textbox via outputing it in the console
+console.log(txtInput)
+//reading the input value 
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked!")
-})
+function clickHandler(){
+    console.log("clicked!");
+    console.log("input" , txtInput.value);
+}
+btnTranslate.addEventListener("click", clickHandler)
+    
